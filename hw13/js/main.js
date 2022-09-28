981320c38bb8db9a22eb08b1ff9e8ce35b75a8e1
IbrahimTs6 <114315719+IbrahimTs6@users.noreply.github.com>
2022-09-28 15:57:07 +0300

    Add files via upload

---
+++ hw13/js/main.js
@@ -0,0 +1,40 @@
+//создаем константу
+const birthday = '14.07.2005';
+//используем тип данных string
+//при попытке изменить содержимое const выдает ошибку
+console.log(birthday);
+
+
+//используем тип данных number
+let p = 3.14;
+console.log(p)
+
+let name = prompt('Имя')
+let addres = prompt('Где живете');
+let phone = prompt('Телефон:');
+
+
+let student = 'Maga Aduev';
+let adres = 'Goygova 17';
+let tele = +79364782621
+console.log(`Студент:${student},адрес:${adres},номер телефона:${tele}`);
+
+//программа
+let $ = 58;
+let stockRu = 3558;
+console.log(stockRu/$);
+
+let a = 5;
+let b = 10;
+b = 9
+c = ++ a + b --;
+console.log(c);
+
+
+
+
+
+
+
+
+
